95ea1c077ba4b3a6352ed725352324a4256af62e
Owen Godshall <ogodshall@gmail.com>
2017-12-10 19:52:04 -0500

    modified filepaths in the keys.js file

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 // Figures out which set of credentials to return (dev or prod)
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return prod keys
-  module.exports = require('prod');
+  module.exports = require('./prod');
 } else {
   // we are in development - return dev keys
-  module.exports = require('dev');
+  module.exports = require('./dev');
 }
