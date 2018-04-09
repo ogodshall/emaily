bd31e127a295b6c32ba2db93cde5e13422190f71
Owen Godshall <ogodshall@gmail.com>
2018-04-08 21:54:18 -0400

    Added front-end routing to the back-end, prepared for Heroku deploy

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Activates the Express server, using Heroku or localhost
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
